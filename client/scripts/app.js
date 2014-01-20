634a805f32fce360f7f789b93a7fb4b7ee0d2c2e
Hack Reactor Students <rhm262@gmail.com>
2014-01-20 15:40:35 -0800

    Changes color scheme and displays most recent messages at set interval

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -22,19 +22,45 @@ $.ajax({
 
 
 // YOUR CODE HERE:
-$.ajax({
-  // always use this url
-  url: 'https://api.parse.com/1/classes/chatterbox',
-  type: 'GET',
-  data: {order: '-createdAt'},
-  contentType: 'application/json',
-  success: function (data) {
-    console.log(data);
-  },
-  error: function (data) {
-    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to retrieve messages');
+var message;
+var getMessages = function(){
+  $.ajax({
+    // always use this url
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    data: {order: '-createdAt'},
+    contentType: 'application/json',
+    success: function (data) {
+      $('.display').empty();
+      displayAllMessages(data);
+      console.log(data);
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to retrieve messages');
+    }
+  });
+};
+
+setInterval(function() {getMessages();},1000);
+//getMessages();
+console.log('here it is:', message);
+
+var displayMessage = function(oneMessage) {
+  var message = oneMessage;
+  var userName = message.username;
+  var text = message.text;
+  var time = message.createdAt;
+  debugger;
+  console.log(userName, " ", text, " ", time);
+  var $display = $(".display");
+  $display.append("<div></div>");
+  $display.find('div').last().text(userName + ': ' + text + ' ' + time);
+};
+
+var displayAllMessages = function(data) {
+  var messages = data.results;
+  for (var i = 0; i < messages.length; i++) {
+    displayMessage(messages[i]);
   }
-});
-
-
+};
