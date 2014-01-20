12fe5db55bb8f39f6c72a13c18d422373602a7ba
Hack Reactor Students <rhm262@gmail.com>
2014-01-20 14:28:39 -0800

    Adds underscore library and GET/POST

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,40 @@
 // YOUR CODE HERE:
+var message = {
+  'username': 'shawn',
+  'text': 'tro',
+  'roomname': '4c'
+};
+
+$.ajax({
+  // always use this url
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  type: 'POST',
+  data: JSON.stringify(message),
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: Message sent');
+  },
+  error: function (data) {
+    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message');
+  }
+});
+
+
+// YOUR CODE HERE:
+$.ajax({
+  // always use this url
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  type: 'GET',
+  data: {order: '-createdAt'},
+  contentType: 'application/json',
+  success: function (data) {
+    console.log(data);
+  },
+  error: function (data) {
+    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to retrieve messages');
+  }
+});
+
+
