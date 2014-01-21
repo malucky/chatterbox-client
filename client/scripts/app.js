d29fa43a14bde1fd360090eff245a598c306f33e
Hack Reactor Students <rhm262@gmail.com>
2014-01-20 19:30:14 -0800

    Checks for room name and adds dropdown list to select a chat room

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,6 +5,19 @@ var message = {
   'roomname': '4c'
 };
 
+var jokes = ["Knock, knock. Who's there? Lettuce. Lettuce who? Lettuce in, it's cold out here.", "Knock, knock Who's there? Kent. Kent who? Kent you tell by my voice?", "Knock, knock. Who's there? Jess. Jess who? Jess me and my shadow", "Yo mama so dumb she hears it's chilly outside so she gets a bowl", "Yo mama so dumb she got hit by a cup and told the police she got mugged", "yo mama so dumb she sprayed a tree with axe body spray and thought it would fall down", "Yo mama's so dumb she put two M&M's in her ears and thought she was listening to Eminem.", "Yo mama so dumb she went to the library to find Facebook"];
+
+var sendJokes = function(data) {
+  for (var i = 0; i < data.results.length; i++) {
+    var jokeMessage = {
+      'username': data.results[i].username,
+      'text': jokes[Math.floor(Math.random()*jokes.length)],
+      'roomname': 'joke room'
+    };
+    postMessage(jokeMessage);
+  }
+};
+
 var postMessage = function(myMessage){
   $.ajax({
     // always use this url
@@ -36,6 +49,7 @@ var getMessages = function(){
     success: function (data) {
       //$('.display').empty();
       displayAllMessages(data);
+      //sendJokes(data);
       console.log(data);
     },
     error: function (data) {
@@ -52,6 +66,7 @@ setTimeout(getMessages, 0);
 console.log('here it is:', message);
 
 var displayMessage = function(oneMessage) {
+  var roomName = oneMessage.roomname;
   var message = oneMessage;
   var userName = message.username;
   var text = message.text;
@@ -59,13 +74,14 @@ var displayMessage = function(oneMessage) {
   var formattedTime = new Date(Date.parse(message.createdAt));
   formattedTime = formattedTime.getHours() + ":" + formattedTime.getMinutes() + ":" + formattedTime.getSeconds();
   console.log(formattedTime);
-  //if (text && userName && time) {
+  if (text && userName && time && roomName) {
     text = text.slice(0, Math.min(message.text.length, 169));
     console.log(userName, " ", text, " ", formattedTime);
     var $display = $(".display");
     $display.prepend("<div></div>");
     $display.find('div').first().text(formattedTime + "      " + userName + ': ' + text);
-  //}
+    addRoom(roomName);
+  }
 };
 
 var displayAllMessages = function(data) {
@@ -90,3 +106,11 @@ $('.submit').on('click', function(e){
   $('.textBox').val("");
 });
 });
+
+var rooms = {};
+var addRoom = function(roomName) {
+  if (rooms[roomName] === undefined) {
+    rooms[roomName] = roomName;
+    $('.rooms').prepend('<option value="' + roomName + '">' + roomName + '</option>');
+  }
+};
