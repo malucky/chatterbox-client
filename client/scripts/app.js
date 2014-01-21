5da07404b91237451a5e13e3316c5823fc3166d8
Hack Reactor Students <rhm262@gmail.com>
2014-01-21 11:39:48 -0800

    Adds room changing functionality with correct messages displaying upon changing rooms and displaying new messages in the correct room

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,11 +1,7 @@
 // YOUR CODE HERE:
-var message = {
-  'username': 'SONALI and Ray',
-  //'text': 'WHAZZZZZUPP!!',
-  'roomname': '4c'
-};
 
-var jokes = ["Knock, knock. Who's there? Lettuce. Lettuce who? Lettuce in, it's cold out here.", "Knock, knock Who's there? Kent. Kent who? Kent you tell by my voice?", "Knock, knock. Who's there? Jess. Jess who? Jess me and my shadow", "Yo mama so dumb she hears it's chilly outside so she gets a bowl", "Yo mama so dumb she got hit by a cup and told the police she got mugged", "yo mama so dumb she sprayed a tree with axe body spray and thought it would fall down", "Yo mama's so dumb she put two M&M's in her ears and thought she was listening to Eminem.", "Yo mama so dumb she went to the library to find Facebook"];
+/****************************jokes *************************/
+/*var jokes = ["Knock, knock. Who's there? Lettuce. Lettuce who? Lettuce in, it's cold out here.", "Knock, knock Who's there? Kent. Kent who? Kent you tell by my voice?", "Knock, knock. Who's there? Jess. Jess who? Jess me and my shadow", "Yo mama so dumb she hears it's chilly outside so she gets a bowl", "Yo mama so dumb she got hit by a cup and told the police she got mugged", "yo mama so dumb she sprayed a tree with axe body spray and thought it would fall down", "Yo mama's so dumb she put two M&M's in her ears and thought she was listening to Eminem.", "Yo mama so dumb she went to the library to find Facebook"];
 
 var sendJokes = function(data) {
   for (var i = 0; i < data.results.length; i++) {
@@ -17,7 +13,12 @@ var sendJokes = function(data) {
     postMessage(jokeMessage);
   }
 };
+*/
 
+var message;
+var lastTime = 0; //time when most recent message was posted
+
+//Ajax POST- submits messages and stringifies them
 var postMessage = function(myMessage){
   $.ajax({
     // always use this url
@@ -35,10 +36,7 @@ var postMessage = function(myMessage){
   });
 };
 
-//setInterval(postMessage, 1000);
-
-// YOUR CODE HERE:
-var message;
+//Ajax GET- retrieves messages and calls displayAllMessages to display all of the messages
 var getMessages = function(){
   $.ajax({
     // always use this url
@@ -47,10 +45,7 @@ var getMessages = function(){
     data: {order: '-createdAt'},
     contentType: 'application/json',
     success: function (data) {
-      //$('.display').empty();
       displayAllMessages(data);
-      //sendJokes(data);
-      console.log(data);
     },
     error: function (data) {
       // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -59,58 +54,87 @@ var getMessages = function(){
   });
 };
 
-var lastTime = 0;
-setInterval(getMessages, 3000);
-setTimeout(getMessages, 0);
-//getMessages();
-console.log('here it is:', message);
-
-var displayMessage = function(oneMessage) {
-  var roomName = oneMessage.roomname;
-  var message = oneMessage;
-  var userName = message.username;
-  var text = message.text;
+/*****************display single message ******************/
+var displayMessage = function(message) {
+  var roomName = scrubbing(message.roomname);
+  var userName = scrubbing(message.username);
+  var text = scrubbing(message.text);
   var time = message.createdAt;
+  var $display = $(".display");
   var formattedTime = new Date(Date.parse(message.createdAt));
   formattedTime = formattedTime.getHours() + ":" + formattedTime.getMinutes() + ":" + formattedTime.getSeconds();
-  console.log(formattedTime);
-  if (text && userName && time && roomName) {
-    text = text.slice(0, Math.min(message.text.length, 169));
-    console.log(userName, " ", text, " ", formattedTime);
-    var $display = $(".display");
-    $display.prepend("<div></div>");
-    $display.find('div').first().text(formattedTime + "      " + userName + ': ' + text);
-    addRoom(roomName);
-  }
+
+  $display.prepend('<div class="' + roomName + '"></div>');
+  $display.find('div').first().text(formattedTime + "      " + userName + ': ' + text);
+  addRoom(roomName);
 };
 
+var scrubbing = function(str){
+  str = str || ""; //If string is undefined, set it to an empty string
+  str = str.slice(0, Math.min(str.length, 169)); //limits the length of a string (text, username, roomname, etc)
+  return str.replace(/<|>/g, ""); //prevents hacking by removing opening tags
+};
+
+/******************display all messages ******************************/
 var displayAllMessages = function(data) {
   var messages = data.results;
+  var currentRoom = $('.rooms').val();
+
+//If the message is more recent than the last message posted, call displayMessage to post it
   for (var i = messages.length - 1; i >= 0; i--) {
-    //debugger;
     if (Date.parse(messages[i].createdAt) > lastTime) {
       displayMessage(messages[i]);
     }
   }
-  lastTime = Date.parse(messages[0].createdAt);
+  lastTime = Date.parse(messages[0].createdAt); //Continue to reset the last time a message was posted
+  changeRoom();
 };
-$(document).ready( function(){
-$('.submit').on('click', function(e){
-  e.preventDefault();
-  var myMessage = {
-    text: $('.textBox').val(),
-    username: window.location.search.slice(10),
-    roomname: 'HR'
-  };
-  postMessage(myMessage);
-  $('.textBox').val("");
-});
-});
 
+
+/****************** room drop down list *****************************/
 var rooms = {};
 var addRoom = function(roomName) {
   if (rooms[roomName] === undefined) {
     rooms[roomName] = roomName;
-    $('.rooms').prepend('<option value="' + roomName + '">' + roomName + '</option>');
+    $('.rooms').append('<option value="' + roomName + '">' + roomName + '</option>'); //create a new roomname on the dropdown list of options
   }
 };
+
+var changeRoom = function() {
+  var roomName = scrubbing($('.rooms').val()); //so this matches the entered/scrubbed roomnames
+  var $messagesToHide = $(".display").find('div');
+  for (var i = 0; i < $messagesToHide.length; i++) {
+    var $message = $($messagesToHide[i]);
+    $message.removeClass('hideMessage'); // remove hideMessage class from all elements to display everything
+    if (roomName === "commonRoom") {
+      continue; //if commonRoom is selected, continue to run through for loop and remove hideMessage class from everything
+    } else if (roomName !== $messagesToHide[i].className) {
+      $message.addClass('hideMessage');
+    }
+  }
+};
+
+
+
+/**************** main *********************/
+$(document).ready( function(){
+  setInterval(getMessages, 3000);
+  setTimeout(getMessages, 0);
+
+  //send message
+  $('.submit').on('click', function(e){
+    e.preventDefault(); //preventing the page from refreshing on click
+    var myMessage = {
+      text: $('.textBox').val(),
+      username: window.location.search.slice(10),
+      roomname: scrubbing($('.rooms').val())
+    };
+    postMessage(myMessage);
+    $('.textBox').val("");
+  });
+
+  //change room
+  $('.rooms').change(function(){
+    changeRoom();
+  });
+});
